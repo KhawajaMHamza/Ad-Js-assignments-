9576cfadcbabfe3614dec2af485cec726b3c3211
KhawajaMHamza <mhamzakhawaja007@gmail.com>
2025-12-08 17:05:29 +0500

    Add files via upload

---
+++ Ad js assigment 2/Ad js assigment 2/app.js
@@ -0,0 +1,228 @@
+// let a = 5; 
+
+// // 1
+// let exp1 = "abc"; 
+
+// // 2
+// let exp2 = "test"; 
+
+// // 3
+// let exp3 = "2value"; 
+// // 4
+// let exp4 = "xyz"; 
+
+// // 5
+// let exp5 = true; 
+
+// // 6
+// let exp6 = 11; 
+
+// // 7
+// let exp7 = 11; 
+
+// // 8
+// let exp8 = 3; 
+
+// // 9
+// let exp9 = "null"; 
+
+// // 10
+// let exp10 = "start"; 
+
+// // 11
+// let exp11 = 3; // 3
+
+// // 12
+// let exp12 = "result"; 
+
+// // 13
+// let exp13 = "test"; 
+
+// // 14
+// let exp14 = 4; 
+
+// // 15
+// let exp15 = "result"; 
+
+// // 16
+// let exp16 = "start"; 
+
+// // 17
+// let exp17 = 0; 
+
+// // 18
+// let exp18 = 0; 
+
+// // 19
+// let exp19 = "foo"; 
+
+// // 20
+// let exp20 = "end5"; 
+
+// // ------ 21 to 42 ------
+
+// // 21
+// let exp21 = "testfalse"; 
+
+// // 22
+// let exp22 = "test"; 
+
+// // 23
+// let exp23 = "result"; 
+
+// // 24
+// let exp24 = 2; 
+
+// // 25
+// let exp25 = 5; 
+
+// // 26
+// let exp26 = 12; 
+
+// // 27
+// let exp27 = "start5"; 
+
+// // 28
+// let exp28 = "world"; 
+
+// // 29
+// let exp29 = "NaNresult"; 
+
+// // 30
+// let exp30 = "falsevalueend"; 
+
+// // 31
+// let exp31 = "3end"; 
+
+// // 32
+// let exp32 = 3; 
+// // 33
+// let exp33 = "end5"; 
+
+// // 34
+// let exp34 = "hello20"; 
+
+// // 35
+// let exp35 = "falseend"; 
+
+// // 36
+// let exp36 = 8; 
+
+// // 37
+// let exp37 = "2test"; 
+
+// // 38
+// let exp38 = "2end"; 
+
+// // 39
+// let exp39 = 2; 
+
+// // 40
+// let exp40 = NaN; 
+
+// // 41
+// let exp41 = "hello"; 
+
+// // 42
+// let exp42 = NaN; 
+
+// ------ 43 to 75 ------
+
+// // 43
+// let exp43 = "start5"; 
+
+// // 44
+// let exp44 = "value1"; 
+
+// // 45
+// let exp45 = 19; 
+
+// // 46
+// let exp46 = "test"; 
+
+// // 47
+// let exp47 = 8; 
+
+// // 48
+// let exp48 = "9hello"; 
+
+// // 49
+// let exp49 = 32; 
+
+// // 50
+// let exp50 = " "; 
+
+// // 51
+// let exp51 = 42; 
+
+// // 52
+// let exp52 = "9test"; 
+
+// // 53
+// let exp53 = "3value"; 
+
+// // 54
+// let exp54 = "7result"; 
+
+// // 55
+// let exp55 = "start25test"; 
+
+// // 56
+// let exp56 = "24test"; 
+
+// // 57
+// let exp57 = "NaN"; 
+
+// // 58
+// let exp58 = "7end3"; 
+
+// // 59
+// let exp59 = "100test"; 
+
+// // 60
+// let exp60 = "5valuetest4"; 
+
+// // 61
+// let exp61 = "8result"; 
+
+// // 62
+// let exp62 = "start16test"; 
+
+// // 63
+// let exp63 = "7test"; 
+
+// // 64
+// let exp64 = "NaN"; 
+
+// // 65
+// let exp65 = "value15test"; 
+
+// // 66
+// let exp66 = "12test"; 
+
+// // 67
+// let exp67 = "start20end"; 
+
+// // 68
+// let exp68 = "12result"; 
+
+// // 69
+// let exp69 = "35test"; 
+
+// // 70
+// let exp70 = "7result"; 
+
+// // 71
+// let exp71 = "test16"; 
+
+// // 72
+// let exp72 = "18value"; 
+
+// // 73
+// let exp73 = "5test"; 
+
+// // 74
+// let exp74 = "10end"; 
+
+// // 75
+// let exp75 = "3test"; 
